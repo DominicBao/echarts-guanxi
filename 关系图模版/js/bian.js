1877a8670fc8fa4e07de15090bd36205dbae800b
DominicBao <41851914+DominicBao@users.noreply.github.com>
2019-11-07 11:59:47 +0800

    Add files via upload

---
+++ 关系图模版/js/bian.js
@@ -0,0 +1,4 @@
+﻿var bian = [{"Source":"xxxxx","Target":"yyyy","info":"20190910"},
+{"Source":"yyyy","Target":"xxxxx","info":"20190910"},
+{"Source":"zzzz","Target":"xxxxx","info":"20190910"}
+]
---
+++ 关系图模版/js/dian.js
@@ -0,0 +1,4 @@
+﻿var dian = [{"id":"xxxxx","cnt":"40590","size":"5"},
+{"id":"yyyy","cnt":"40590","size":"5"},
+{"id":"zzzz","cnt":"40590","size":"5"}
+]
